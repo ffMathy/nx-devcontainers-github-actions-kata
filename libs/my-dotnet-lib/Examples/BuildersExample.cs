3bf14740f1d23b74ea762933d9386fb326d66842
ffMathy <mathias.lorenzen@live.com>
2025-11-01 08:32:10 +0000

    feat: add examples

---
+++ libs/my-dotnet-lib/Examples/BuildersExample.cs
@@ -0,0 +1,77 @@
+class User
+{
+  public string Name { get; set; }
+  public int Age { get; set; }
+}
+
+class UserBuilder
+{
+  private string name;
+  private int age;
+
+  public UserBuilder WithName(string name)
+  {
+    this.name = name;
+    return this;
+  }
+
+  public UserBuilder WithAge(int age)
+  {
+    this.age = age;
+    return this;
+  }
+
+  public virtual Task<User> CreateAsync()
+  {
+    if (string.IsNullOrEmpty(name))
+    {
+      throw new InvalidOperationException("Name must be provided");
+    }
+
+    return Task.FromResult(new User
+    {
+      Name = name,
+      Age = age
+    });
+  }
+}
+
+class TestUserBuilder : UserBuilder
+{
+  private bool shouldEmailBeValidated = false;
+
+  public TestUserBuilder()
+  {
+    WithName(Guid.NewGuid().ToString());
+    WithAge(30);
+  }
+
+  public TestUserBuilder WithValidatedEmail()
+  {
+    shouldEmailBeValidated = true;
+    return this;
+  }
+
+  public override async Task<User> CreateAsync()
+  {
+    if (shouldEmailBeValidated)
+    {
+      //TODO: create the necessary entities to simulate that the email activation link has been pressed
+    }
+
+    return await base.CreateAsync();
+  }
+}
+
+class Program2
+{
+  static async Task Main(string[] args)
+  {
+    User user = await new TestUserBuilder()
+      .WithValidatedEmail()
+      .WithName("Alice")
+      .CreateAsync();
+
+    Console.WriteLine($"Created user: {user.Name}, Age: {user.Age}");
+  }
+}
---
+++ libs/my-dotnet-lib/Examples/TestContextExample.cs
@@ -0,0 +1,109 @@
+class FrontendTestContext : IAsyncDisposable
+{
+  public UniooBankApiTestContext BankApi { get; }
+
+  public async Task Initialize()
+  {
+    await BankApi.Initialize();
+    //TODO: run "angular start" for frontend
+    //TODO: spin up Playwright browser
+  }
+
+  public async ValueTask DisposeAsync()
+  {
+    await BankApi.DisposeAsync();
+    //TODO: kill angular
+    //TODO: kill Playwright browser
+  }
+
+  public async Task<FrontendFrontPageTestContext> NavigateToFrontPage()
+  {
+    //TODO: navigate to front page
+    return new FrontendFrontPageTestContext();
+  }
+
+  public async Task<FrontendSearchPageTestContext> NavigateToSearchPage()
+  {
+    //TODO: navigate to search page
+    return new FrontendSearchPageTestContext();
+  }
+}
+
+class UniooBankApiTestContext : IAsyncDisposable
+{
+  public DatabaseTestContext Database { get; }
+  public WebhookTestContext Webhooks { get; }
+
+  public async Task Initialize()
+  {
+    await Database.Initialize();
+    await Webhooks.Initialize();
+  }
+
+  public async ValueTask DisposeAsync()
+  {
+    await Database.DisposeAsync();
+    await Webhooks.DisposeAsync();
+  }
+
+  public async Task MockAuthenticationForUserAsync(User user)
+  {
+    //TODO: mock the authentication system to simulate that the given user is logged in
+  }
+}
+
+class DatabaseTestContext : IAsyncDisposable
+{
+  public TestUserBuilder UserBuilder { get; set; }
+
+  public async Task Initialize()
+  {
+  }
+
+  public async ValueTask DisposeAsync()
+  {
+  }
+}
+
+class WebhookTestContext : IAsyncDisposable
+{
+  public async Task Initialize()
+  {
+  }
+
+  public async ValueTask DisposeAsync()
+  {
+  }
+}
+
+class FrontendSearchPageTestContext
+{
+  public async Task PerformSearch(string query)
+  {
+    //TODO: write in search field and hit enter
+  }
+}
+
+class FrontendFrontPageTestContext
+{
+}
+
+class Program1
+{
+  static async Task Main(string[] args)
+  {
+    await using var testContext = new FrontendTestContext();
+    await testContext.Initialize();
+
+    var someUser = await testContext.BankApi.Database.UserBuilder
+      .WithAge(24)
+      .CreateAsync();
+
+    await testContext.BankApi.MockAuthenticationForUserAsync(someUser);
+
+    var frontPage = await testContext.NavigateToFrontPage();
+
+    var searchPage = await testContext.NavigateToSearchPage();
+    await searchPage.PerformSearch("test query");
+  }
+}
